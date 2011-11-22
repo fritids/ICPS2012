ae3bebad8f476168f98bb3e900ac0c0f793eb90b
Joren <jorenb@gmail.com>
2011-11-22 20:24:27 +0100

    users can now register into the database. no emails being sent yet, and no control panel.

--- functions/register.php
+++ functions/register.php
@@ -6,6 +6,7 @@ function icps_register_user($data) {
     $userdata = array(
         'first_name' => $_POST['first_name'],
 	'last_name' => $_POST['last_name'],
+        'display_name' => $_POST['first_name'] . ' ' . $_POST['last_name'],
 	'email' => $_POST['email'],
 	'country' => $_POST['country'],
 	'university' => $_POST['university'],
@@ -14,7 +15,36 @@ function icps_register_user($data) {
 	'contribute' => $_POST['contribute']
     );
 
-    array_walk($userdata, esc_attr);
+    $safedata = array_map('mysql_real_escape_string', $userdata);
 
-    
+    if( !is_email($safedata['email']) ) return array(false, 1);
+
+    $username = $safedata['email'];
+    if( username_exists( $username ) ) return array(false, 2);
+
+    $random_pw = wp_generate_password();
+    $user_id = wp_create_user( $username, $random_pw, $safedata['email'] );
+
+    if(! wp_update_user( array(
+        'ID' => $user_id,
+        'first_name' => $safedata['first_name'],
+        'last_name' => $safedata['last_name'],
+        'display_name' => $safedata['display_name']
+        )) 
+    ) :
+        wp_delete_user( $user_id );
+	return array(false, 99);
+    endif; // update basic user info
+        
+
+    $user_metas = array('country', 'university', 'study', 'level', 'contribute');
+
+    foreach($user_metas as $field) :
+        if(!update_user_meta( $user_id, $field, $safedata[$field] ) ) {
+            wp_delete_user( $user_id );
+	    return array(false, 99);
+        }
+    endforeach; // user meta fields
+
+    return array(true, $user_id);
 }
--- register.php
+++ register.php
@@ -3,17 +3,37 @@
  * Template Name: Register
  */
 
-
+$errors = array();
 if ('POST' == $_SERVER['REQUEST_METHOD'] && wp_verify_nonce($_POST['icnonsense'], 'register') ) :
     require 'functions/register.php';
 
     $result = icps_register_user($_POST);
+    $user_id = null;
+
+    if($result[0] === false) : //something went wrong!
+        $errors[] = $result[1];
+
+    else :
+        $user_id = $result[1];
+    endif; // did an error occur?
+elseif ('POST' == $_SERVER['REQUEST_METHOD'] && !wp_verify_nonce($_POST['icnonsense'], 'register') ) :
+    die("You don\'t seem to have arrived here from the right page. For security reasons, we cannot accept the supplied data.");
 endif; // has registration data been supplied in a valid way?
 
 wp_enqueue_style('register');
 
 get_header();
 
+if( empty($errors) && isset($user_id) ) : // have we actually registered a user?
+    ?>
+    <div id="content">
+        <p>Thank you for your application. An email has been sent to the supplied address, in which you will find your password for logging on to your user profile.</p>
+    </div><!-- #content -->
+
+    <div class="clear"></div>
+    <?php
+
+else :
 ?>
 
 <div id="content">
@@ -72,4 +92,5 @@ get_header();
 <div class="clear"></div>
 <?php
 
+endif; // has valid data been supplied?
 get_footer();
